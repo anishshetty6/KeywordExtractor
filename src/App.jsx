30d3a29ad819c6ba3690951b2b0bbd14e93e7287
anishshetyy6 <anishshetty2708@gmail.com>
2024-06-23 17:26:12 +0530

    open ai api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import { useState } from 'react';
+import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
 
 const App = () => {
-  return (
-    <>
-    <h1>my app</h1>
-    </>
-  )
-}
+  const [keywords, setKeywords] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-export default App
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        messages: [
+          {
+            role: 'user',
+            content:
+              'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas\n\n' +
+              text,
+          },
+        ],
+        model: 'gpt-3.5-turbo'
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_OPENAI_API_URL,
+        options
+      );
+      const json = await response.json();
+      console.log(json.choices[0].text.trim());
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <Container maxW='3xl' centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
+    </Box>
+  );
+};
+
+export default App;
